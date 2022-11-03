7880f2a8566fae1d2e2193c7e6eb348d17758d75
nozomi-iida <iida19990106@gmail.com>
2022-11-03 19:35:22 +0900

    fix: custom background color

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -30,126 +30,128 @@ export const Header = () => {
   const [isSearchOpen, setIsSearchOpen] = useBoolean();
 
   return (
-    <Flex margin="auto" maxW={970} px={15} justify="space-between">
-      <Link href="">
-        <Heading lineHeight="100px">Nozo blog</Heading>
-      </Link>
-      <Flex gap={10}>
-        <SlideFade in={isMenuOpen} offsetX={5} offsetY={0}>
-          <Flex align="center" fontWeight="bold" h="full" gap={5}>
-            <Link href="">
-              <Text transition="color 0.2s" _hover={{ color: "blue.400" }}>
-                Engineer
-              </Text>
-            </Link>
-            <Link href="">
-              <Text transition="color 0.2s" _hover={{ color: "blue.400" }}>
-                Life
-              </Text>
-            </Link>
-            <Link href="">
-              <Text transition="color 0.2s" _hover={{ color: "blue.400" }}>
-                About
-              </Text>
-            </Link>
-          </Flex>
-        </SlideFade>
-        <Flex>
-          <Center
-            w={12}
-            as="a"
-            href="#"
-            transition="color 0.2s"
-            _hover={{ color: "blue.400" }}
-            color={isMenuOpen ? "blue.400" : undefined}
-            onClick={setIsMenuOpen.toggle}
-          >
-            <Box
-              w={4}
-              h="3px"
-              backgroundColor="currentcolor"
-              position="absolute"
-              _before={{
-                h: "3px",
-                w: isMenuOpen ? 2 : 4,
-                mx: isMenuOpen ? 1 : 0,
-                content: `""`,
-                display: "block",
-                backgroundColor: "currentcolor",
-                position: "absolute",
-                top: "-7px",
-                transition: "all 0.2s",
-              }}
-              _after={{
-                h: "3px",
-                w: isMenuOpen ? 2 : 4,
-                mx: isMenuOpen ? 1 : 0,
-                content: `""`,
-                display: "block",
-                backgroundColor: "currentcolor",
-                position: "absolute",
-                top: "7px",
-                transition: "all 0.2s",
-              }}
-            />
-          </Center>
-          <Center
-            w={12}
-            as="a"
-            href="#"
-            transition="color 0.2s"
-            _hover={{ color: "blue.400" }}
-          >
-            <SunIcon w={17} h={17} />
-          </Center>
-
-          <Popover isOpen={isSearchOpen}>
-            <PopoverTrigger>
-              <Center
-                w={12}
-                as="a"
-                href="#"
-                transition="color 0.2s"
-                color={isSearchOpen ? "blue.400" : undefined}
-                _hover={{ color: "blue.400" }}
-                onClick={setIsSearchOpen.toggle}
-              >
-                <Box>
-                  <Fade in={isSearchOpen} hidden={!isSearchOpen}>
-                    <CloseIcon w={17} h={17} />
-                  </Fade>
-                  <Fade in={!isSearchOpen} hidden={isSearchOpen}>
-                    <SearchIcon w={17} h={17} />
-                  </Fade>
-                </Box>
-              </Center>
-            </PopoverTrigger>
-            <PopoverContent
-              onClick={(e) => e.stopPropagation()}
-              backgroundColor="black"
-              color="gray"
-              py={6}
-              px={7}
+    <Box backgroundColor="white">
+      <Flex margin="auto" maxW={970} px={15} justify="space-between">
+        <Link href="">
+          <Heading lineHeight="100px">Nozo blog</Heading>
+        </Link>
+        <Flex gap={10}>
+          <SlideFade in={isMenuOpen} offsetX={5} offsetY={0}>
+            <Flex align="center" fontWeight="bold" h="full" gap={5}>
+              <Link href="">
+                <Text transition="color 0.2s" _hover={{ color: "blue.400" }}>
+                  Engineer
+                </Text>
+              </Link>
+              <Link href="">
+                <Text transition="color 0.2s" _hover={{ color: "blue.400" }}>
+                  Life
+                </Text>
+              </Link>
+              <Link href="">
+                <Text transition="color 0.2s" _hover={{ color: "blue.400" }}>
+                  About
+                </Text>
+              </Link>
+            </Flex>
+          </SlideFade>
+          <Flex>
+            <Center
+              w={12}
+              as="a"
+              href="#"
+              transition="color 0.2s"
+              _hover={{ color: "blue.400" }}
+              color={isMenuOpen ? "blue.400" : undefined}
+              onClick={setIsMenuOpen.toggle}
             >
-              <PopoverArrow backgroundColor="black" />
-              <PopoverBody>
-                <InputGroup>
-                  {/* autoFocusできるようにしたい */}
-                  <Input
-                    autoFocus
-                    variant="flushed"
-                    placeholder="Enter your search query..."
-                    _focusVisible={{ borderColor: "gray.200" }}
-                  />
-                  <InputRightElement>
-                    <SearchIcon />
-                  </InputRightElement>
-                </InputGroup>
-              </PopoverBody>
-            </PopoverContent>
-          </Popover>
+              <Box
+                w={4}
+                h="3px"
+                backgroundColor="currentcolor"
+                position="absolute"
+                _before={{
+                  h: "3px",
+                  w: isMenuOpen ? 2 : 4,
+                  mx: isMenuOpen ? 1 : 0,
+                  content: `""`,
+                  display: "block",
+                  backgroundColor: "currentcolor",
+                  position: "absolute",
+                  top: "-7px",
+                  transition: "all 0.2s",
+                }}
+                _after={{
+                  h: "3px",
+                  w: isMenuOpen ? 2 : 4,
+                  mx: isMenuOpen ? 1 : 0,
+                  content: `""`,
+                  display: "block",
+                  backgroundColor: "currentcolor",
+                  position: "absolute",
+                  top: "7px",
+                  transition: "all 0.2s",
+                }}
+              />
+            </Center>
+            <Center
+              w={12}
+              as="a"
+              href="#"
+              transition="color 0.2s"
+              _hover={{ color: "blue.400" }}
+            >
+              <SunIcon w={17} h={17} />
+            </Center>
+
+            <Popover isOpen={isSearchOpen}>
+              <PopoverTrigger>
+                <Center
+                  w={12}
+                  as="a"
+                  href="#"
+                  transition="color 0.2s"
+                  color={isSearchOpen ? "blue.400" : undefined}
+                  _hover={{ color: "blue.400" }}
+                  onClick={setIsSearchOpen.toggle}
+                >
+                  <Box>
+                    <Fade in={isSearchOpen} hidden={!isSearchOpen}>
+                      <CloseIcon w={17} h={17} />
+                    </Fade>
+                    <Fade in={!isSearchOpen} hidden={isSearchOpen}>
+                      <SearchIcon w={17} h={17} />
+                    </Fade>
+                  </Box>
+                </Center>
+              </PopoverTrigger>
+              <PopoverContent
+                onClick={(e) => e.stopPropagation()}
+                backgroundColor="black"
+                color="gray"
+                py={6}
+                px={7}
+              >
+                <PopoverArrow backgroundColor="black" />
+                <PopoverBody>
+                  <InputGroup>
+                    {/* autoFocusできるようにしたい */}
+                    <Input
+                      autoFocus
+                      variant="flushed"
+                      placeholder="Enter your search query..."
+                      _focusVisible={{ borderColor: "gray.200" }}
+                    />
+                    <InputRightElement>
+                      <SearchIcon />
+                    </InputRightElement>
+                  </InputGroup>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </Box>
   );
 };
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Box minH="100vh" backgroundColor="blackAlpha.50">
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   );
 }
