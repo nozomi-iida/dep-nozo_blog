9fd3ea8d3cf1e0266fc79fa7de03237023474292
nozomi-iida <iida19990106@gmail.com>
2022-11-15 22:59:30 +0900

    fix: head

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import chakraTheme from "../libs/chakra/theme";
 import { NextPage } from "next";
 import { ReactElement, ReactNode } from "react";
+import Head from "next/head";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,10 +16,19 @@ type AppPropsWithLayout = AppProps & {
 export const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <ChakraProvider theme={chakraTheme}>
-      <ColorModeScript initialColorMode={chakraTheme.config.theme} />
-      {getLayout(<Component {...pageProps} />)}
-    </ChakraProvider>
+    <div>
+      <Head>
+        <title>Nozo Blog</title>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <ChakraProvider theme={chakraTheme}>
+        <ColorModeScript initialColorMode={chakraTheme.config.theme} />
+        {getLayout(<Component {...pageProps} />)}
+      </ChakraProvider>
+    </div>
   );
 };
 
