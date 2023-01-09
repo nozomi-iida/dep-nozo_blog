9df1dc2a16cc093db4aabc4240cfdad4e6c9abd9
nozomi-iida <iida19990106@gmail.com>
2023-01-09 22:31:20 +0900

    test: create topic factory

---
+++ go_api/test/factories/topic.go
@@ -0,0 +1,27 @@
+package factories
+
+import (
+	"testing"
+
+	"github.com/nozomi-iida/nozo_blog/domain/topic/sqlite"
+	"github.com/nozomi-iida/nozo_blog/entity"
+)
+
+type topicOptions func(*entity.Topic)
+
+func SetTopicName(name string) topicOptions {
+	return func(t *entity.Topic) {
+		t.SetName(name)
+	}	
+}
+
+func CreateTopic(t *testing.T, fileName string, options ...topicOptions) entity.Topic  {
+	topic, err := entity.NewTopic(entity.Topic{Name: "test topic", Description: "description"})	
+	sq, err := sqlite.New(fileName)
+	_, err = sq.Create(topic)
+	if err != nil {
+		t.Error("create user err:", err)
+	}
+
+	return topic
+}
